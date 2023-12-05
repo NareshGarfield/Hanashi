aeea2b9b652c502db542b1c0161a8709da829988
NareshGarfield <nareshgarfield@gmail.com>
2023-12-05 16:32:09 +0530

    fixed finaly

--- next.config.js
+++ next.config.js
@@ -1,24 +1,17 @@
-module.exports = {
-    reactStrictMode: true,
-    images: {
-      domains: [
-        "res.cloudinary.com",
-        "avatars.githubusercontent.com",
-        "lh3.googleusercontent.com",
-        "i.pinimg.com"
-      ]
-    },
-    async headers() {
-      return [
-        {
-          source: '/(.*)',
-          headers: [
-            {
-              key: 'Pooz',
-              value: 'Hanashi'
-            }
-          ]
-        }
-      ]
-    }
-  };
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    appDir: true,
+    swcPlugins: [["next-superjson-plugin", {}]]
+  },
+  images: {
+    domains: [
+      'res.cloudinary.com', 
+      'avatars.githubusercontent.com',
+      'lh3.googleusercontent.com',
+      'i.pinimg.com'
+    ]
+  }
+}
+
+module.exports = nextConfig
